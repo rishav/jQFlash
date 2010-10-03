dd62350b469ca44ba1e668f87f2c839527bd12de
Rishav Rastogi <rishav.rastogi@gmail.com>
2010-10-03 13:48:14 +0530

    making it extensible

--- jqflash.js
+++ jqflash.js
@@ -1,11 +1,18 @@
-	jQuery.fn.flash = function() {
+	jQuery.fn.flash = function(options) {
 		return this.each( function() {
 			var $this = jQuery(this)
 			$this.hide();
-			$this.css({"top":"0px","width":"100%","left":"0px","position":"fixed","z-index":100,"text-align":"center"})
-			$this.append("(Click To hide)")
+			var defaults = {
+				css :{	"top":"0px","width":"100%","left":"0px","position":"fixed","z-index":100,"text-align":"center"},
+				message: "(Click To hide)",
+				hideEvent:"click"
+			}
+			
+			var opts = jQuery.extend(css_defaults, options["css"])
+			$this.css(opts["css"])
+			$this.append(opts["message"])
 			$this.slideToggle();
-			$this.click(function(){
+			$this.bind(opts["hideEvent"],function(){
 				$this.slideToggle()
 			})
 		})

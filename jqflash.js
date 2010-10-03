182653612792e0f01cfa99ca928d5fbeed9aa13f
Rishav Rastogi <rishav.rastogi@gmail.com>
2010-10-03 13:56:19 +0530

    added small demo

--- jqflash.js
+++ jqflash.js
@@ -7,8 +7,10 @@
 				message: "(Click To hide)",
 				hideEvent:"click"
 			}
-			
-			var opts = jQuery.extend(css_defaults, options["css"])
+			if(options != null)
+				var opts = jQuery.extend(defaults, options)
+			else
+			 	var opts = defaults
 			$this.css(opts["css"])
 			$this.append(opts["message"])
 			$this.slideToggle();
